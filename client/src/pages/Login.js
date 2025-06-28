f2c960fa9a57ed943a1ff0d8bc09ab8b00d7c175
DanielAkkina <danielakkina@gmail.com>
2025-06-28 14:39:50 +0530

    🔧 Use env API URL for login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -11,13 +11,23 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ username, password })
-      });
+      // console.log(`${process.env.REACT_APP_API_URL}/api/auth/login`);
+
+      // const res = await fetch("http://localhost:5000/api/auth/login", {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json"
+      //   },
+      //   body: JSON.stringify({ username, password })
+      // });
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({ username, password })
+});
+
 
       const data = await res.json();
 
